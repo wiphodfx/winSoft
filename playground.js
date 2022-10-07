8e5ca1b9f23ab9627249a0071ce8ce95b09cccc5
wiphodfx <wifidifx@gmail.com>
2022-10-07 10:04:56 +0100

    New committed

--- playground.js
+++ playground.js
@@ -14,11 +14,32 @@ const letterFrequency = (phrase) => {
 // console.log(letterFrequency("lol who are you lol lol are you"))
 
 
-let hour = 19
+// let hour = 19
 
-if (hour >= 6 && hour < 12)
-    console.log('Goodmorning')
- else if (hour >= 12 && hour < 18)
-    console.log('good afternoon')
-else
-    console.log('good evening')
+// if (hour >= 6 && hour < 12)
+    // console.log('Goodmorning')
+//  else if (hour >= 12 && hour < 18)
+    // console.log('good afternoon')
+// else
+    // console.log('good evening')
+    // const doubleMap = (numbers) => {
+    //     return numbers.map(number => number * 2)
+    // }
+    // console.log(doubleMap([1, 2, 3, 4]))
+
+    // let result = [1, 2, 3, 4, 5].map(number => number * 2)
+    // console.log(result)
+
+
+    const filter = (numbers, greaterThan) => {
+        let result = []
+
+        for (const number of numbers){
+            if (number > greaterThan){
+                result.push(number)
+            }
+        }
+        return result
+    }
+    const nums = [1, 2, 3, 4, 5, 6]
+    console.log(nums.filter(nums => nums > 3 || nums < 3))
